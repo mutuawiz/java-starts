6ed678f959b0e7b67a7ef2ffbfae419bc3e088a5
mutuawiz <jasonmakau9@gmail.com>
2022-02-12 12:31:09 +0300

    variables

---
+++ src/com/company/simple.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class simple {
+
+   /* static  public void main(String[] args) {
+        int a=10;
+        int b=10;
+        int c=a+b;
+        System.out.println(c);
+
+    }
+}*/
+
+    //variable widening
+ /* public static void main(String[] args){
+        int a=10;
+        float f=a;
+        System.out.println(a);
+        System.out.println(f);
+        }} */
+
+        //typcasting // narrowing
+
+        public static void main(String[] args){
+                float f=10.5f;
+                int a=(int)f;
+                System.out.println(f);
+                System.out.println(a);
+        }}
